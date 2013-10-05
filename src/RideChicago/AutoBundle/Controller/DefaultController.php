b40593e03a67c98e4496ef235c591008e1db4ff8
Matt Lo <mattlo.developer@gmail.com>
2013-10-04 19:10:08 -0500

    added static pages

--- src/RideChicago/AutoBundle/Controller/DefaultController.php
+++ src/RideChicago/AutoBundle/Controller/DefaultController.php
@@ -6,8 +6,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller {
     public function indexAction() {
-        return $this->render('RideChicagoAutoBundle:Default:index.html.twig');
+        return $this->render('RideChicagoAutoBundle:Pages/Public:homepage.html.twig');
     }
+	
+	public function classesAction() {
+		return $this->render('RideChicagoAutoBundle:Pages/Public:homepage.html.twig');
+	}
+	
+	public function scheduleAction() {
+		return $this->render('RideChicagoAutoBundle:Pages/Public:homepage.html.twig');
+	}
+	
+	public function permitsAction() {
+		return $this->render('RideChicagoAutoBundle:Pages/Public:homepage.html.twig');
+	}
+	
+	public function pricingAction() {
+		return $this->render('RideChicagoAutoBundle:Pages/Public:pricing.html.twig');
+	}
+	
+	public function faqAction() {
+		return $this->render('RideChicagoAutoBundle:Pages/Public:faq.html.twig');
+	}
+	
+	public function rcaContact() {
+		return $this->render('RideChicagoAutoBundle:Pages/Public:homepage.html.twig');
+	}
 }
 
 ?>
