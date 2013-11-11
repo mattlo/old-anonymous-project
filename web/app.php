95b8d7ab9c7a59d10c8fc4c5998108886249ca0f
Matt Lo <mattlo.developer@gmail.com>
2013-11-10 21:26:00 -0600

    added prod environment switcher

--- web/app.php
+++ web/app.php
@@ -5,26 +5,37 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-//$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-$loader = require_once __DIR__.'/../app/autoload.php';
+// @TODO have better auto-env handling
+if (file_exists(__DIR__.'/../app/config/env.prod') === true) {
+	$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
-/*
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
-*/
+	// Use APC for autoloading to improve performance.
+	// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
+	// with other applications also using APC.
+	/*
+	$loader = new ApcClassLoader('sf2', $loader);
+	$loader->register(true);
+	*/
 
-require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+	require_once __DIR__.'/../app/AppKernel.php';
+	//require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('dev', true);
-//$kernel = new AppKernel('prod', false);
-//$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
-Request::enableHttpMethodParameterOverride();
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+	$kernel = new AppKernel('prod', false);
+	$kernel->loadClassCache();
+	//$kernel = new AppCache($kernel);
+	Request::enableHttpMethodParameterOverride();
+	$request = Request::createFromGlobals();
+	$response = $kernel->handle($request);
+	$response->send();
+	$kernel->terminate($request, $response);
+
+} else {
+	$loader = require_once __DIR__.'/../app/autoload.php';
+	require_once __DIR__.'/../app/AppKernel.php';
+	$kernel = new AppKernel('dev', true);
+	Request::enableHttpMethodParameterOverride();
+	$request = Request::createFromGlobals();
+	$response = $kernel->handle($request);
+	$response->send();
+	$kernel->terminate($request, $response);
+}
