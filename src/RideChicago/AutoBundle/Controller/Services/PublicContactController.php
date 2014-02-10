414baedbc4d53535611cefe3f715e577a8881699
Matt Lo <mattlo.developer@gmail.com>
2014-02-10 01:02:51 -0600

    fixed comment typo

--- src/RideChicago/AutoBundle/Controller/Services/PublicContactController.php
+++ src/RideChicago/AutoBundle/Controller/Services/PublicContactController.php
@@ -16,7 +16,7 @@ class PublicContactController extends Controller {
     $message = \Swift_Message::newInstance()
 			->setSubject('RideChicago Contact Request')
 			->setFrom($request->request->get('email'))
-			->setTo('drive@ridechicago.com') // @TODO make this configable
+			->setTo('drive@ridechicago.com') // @TODO make this configurable
 			->setBody(
 				$this->renderView(
 					'RideChicagoAutoBundle:Default:contact-form.txt.twig',
