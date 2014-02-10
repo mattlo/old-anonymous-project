96f5dfbbd845e0f8b94a8689394d138da4da7968
Matt Lo <mattlo.developer@gmail.com>
2014-02-10 00:17:26 -0600

    added mailer service

---
+++ src/RideChicago/AutoBundle/Controller/Services/PublicContactController.php
@@ -0,0 +1,36 @@
+<?php
+namespace RideChicago\AutoBundle\Controller\Services;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class PublicContactController extends Controller {
+	
+	/**
+	 * @param \Symfony\Component\HttpFoundation\Request $request
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+	public function sendAction(Request $request) {
+		
+    $message = \Swift_Message::newInstance()
+			->setSubject('RideChicago Contact Request')
+			->setFrom($request->request->get('email'))
+			->setTo('mattlo.developer@gmail.com') // @TODO make this configable
+			->setBody(
+				$this->renderView(
+					'RideChicagoAutoBundle:Default:contact-form.txt.twig',
+					array(
+						'name' => $request->request->get('name'),
+						'email' => $request->request->get('email'),
+						'subject' => $request->request->get('subject'),
+						'message' => $request->request->get('message')
+					)
+				)
+			)
+		;
+		$this->get('mailer')->send($message);
+		
+		return new Response('');
+	}
+}
